faf837162837653d97b43d4da1dbf292af1b9031
sykrabadi <muhammad.syukur.1905356@students.um.ac.id>
2021-05-10 22:09:49 +0700

    change filename from latihan2 to messages

---
+++ messages.py
@@ -0,0 +1,12 @@
+class messages():
+	def n_value_error():
+		return "Number of segment/trapezoid must be a number"
+
+	def lower_bound_error():
+		return "Lower bound must be a number"
+
+	def upper_bound_error():
+		return "Upper bound must be a number"
+
+	def expression_error():
+		return "Unable to continue, please retype the expression in term of x"
